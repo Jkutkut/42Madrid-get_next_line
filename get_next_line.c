c45bf67ac5ae934c608bf69b0cd89f134351ca70
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-28 13:32:56 +0100

    let's try to store the cache on a single char*

--- get_next_line.c
+++ get_next_line.c
@@ -6,16 +6,43 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 10:05:07 by jkutkut           #+#    #+#             */
-/*   Updated: 2022/01/28 12:02:02 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/01/28 13:32:00 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+ssize_t	readChunk(char **cache, int fd)
+{
+	ssize_t	r;
+	char	txt[BUFFER_SIZE + 1];
+	char *oldcache;
+
+	r = read(fd, txt, BUFFER_SIZE);
+	txt[r] = '\0';
+	
+	oldcache = *cache;
+	*cache = ft_strjoin(oldcache, txt);
+	free(oldcache);
+	return r;
+}
+
+char	*ft_getline(char **cache)
+{
+	// char	*line;
+	return (*cache);
+}
+
+
 char	*get_next_line(int fd)
 {
-	char	*txt;
-	txt = malloc(sizeof(char) * BUFFER_SIZE);
-	read(fd, txt, BUFFER_SIZE);
-	return (txt);
+	ssize_t	r;
+	static char	*cache = NULL;
+
+	r = readChunk(&cache, fd);
+	printf("line: %s\n", ft_getline(&cache));
+	printf("chars red: %ld\n", r);
+	// printf("\n***********\n- fd: %d\n- txt: \"%s\"\n- bs: %u\n***********\n",
+			// fd, cache, BUFFER_SIZE);
+	return (NULL);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 10:00:44 by jkutkut           #+#    #+#             */
-/*   Updated: 2022/01/28 13:02:29 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/01/28 13:29:33 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,16 @@
 
 typedef struct s_chunk
 {
-	void			*content;
+	char			*content;
 	struct s_chunk	*next;
 }					t_chunk;
 
-char *get_next_line(int fd);
-t_chunk	*ft_lstnew(char *content);
-t_chunk	*ft_lstlast(t_chunk *lst);
-void	ft_lstadd_back(t_chunk **lst, t_chunk *new);
+char	*get_next_line(int fd);
+
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 char	*ft_strdup(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
 
