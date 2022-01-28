a4d82bd8f1dac078051f8cf24f0469f8135da9bf
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-28 12:01:12 +0100

    test to read lines of file added

--- get_next_line_main.c
+++ get_next_line_main.c
@@ -3,14 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_main.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jkutkut <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 10:37:34 by jkutkut           #+#    #+#             */
-/*   Updated: 2022/01/28 10:37:52 by jkutkut          ###   ########.fr       */
+/*   Updated: 2022/01/28 12:01:07 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <fcntl.h>
+#include "get_next_line.h"
+
 int	main(void)
 {
+	int f = open("get_next_line_main.c", O_RDWR);
+
+	printf("l: \"%s\"", get_next_line(f));
+	printf("l: \"%s\"", get_next_line(f));
+	
 	return (0);
 }
