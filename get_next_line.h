08dd29acfeb1886e7381aa9fe9852a5b7a27784c
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-28 10:04:06 +0100

    allowed functions included

--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,17 @@
 /*   By: jkutkut <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 10:00:44 by jkutkut           #+#    #+#             */
-/*   Updated: 2022/01/28 10:02:37 by jkutkut          ###   ########.fr       */
+/*   Updated: 2022/01/28 10:03:48 by jkutkut          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 #define GET_NEXT_LINE_H
 
+#include <unistd.h>
 #include <stdlib.h>
 
 char *get_next_line(int fd);
 
 #endif
+
