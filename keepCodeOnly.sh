79894c96e886fffbdbc9aee2bdecc6bd22b67174
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-24 14:09:45 +0100

    remove

--- keepCodeOnly.sh
+++ keepCodeOnly.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
 
-dirsToRemove=".git .test_main .test_files gnlTester"
-filesToRemove=".gitignore MakeFile"
+removeDebug() {
+	# dirsToRemove=".git .test_main .test_files gnlTester"
+	# filesToRemove=".gitignore MakeFile"
 
-echo "Removing directories:\n$dirsToRemove"
-rm -rf $dirsToRemove
+	# echo "Removing directories:\n$dirsToRemove"
+	# rm -rf $dirsToRemove
 
-echo "Removing files"
-rm $filesToRemove
+	# echo "Removing files"
+	# rm $filesToRemove
+}
+
+removeCode() {
+	echo "Bonus o Mandatory? [B/M]"
+	read response -n1
+
+	case $response in
+		b|B)
+			echo "Bonus"
+			;;
+		m|M)
+			echo "Mandatory"
+			;;
+		*)
+			echo "Error"
+	esac
+
+	# rm $0
+}
 
 echo "Removing script"
 rm $0
