099dab4e96aa29e97e643be90181d6328f06fdf7
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-24 15:06:12 +0100

    this should be almost done

--- keepCodeOnly.sh
+++ keepCodeOnly.sh
@@ -1,33 +1,40 @@
 #!/bin/bash
 
 removeDebug() {
-	# dirsToRemove=".git .test_main .test_files gnlTester"
-	# filesToRemove=".gitignore MakeFile"
+	dirsToRemove=".git .test_main .test_files gnlTester"
+	filesToRemove=".gitignore MakeFile"
 
-	# echo "Removing directories:\n$dirsToRemove"
-	# rm -rf $dirsToRemove
+	echo "Removing directories:\n$dirsToRemove"
+	rm -rf $dirsToRemove
 
-	# echo "Removing files"
-	# rm $filesToRemove
+	echo "Removing files"
+	rm $filesToRemove
 }
 
-removeCode() {
-	echo "Bonus o Mandatory? [B/M]"
+cleanRepo() {
+	echo "Keep Bonus o Mandatory? [B/M]"
 	read response -n1
 
 	case $response in
 		b|B)
 			echo "Bonus"
+			for i in $(ls *bonus*); do
+				f=$(echo $i | sed 's/_bonus//')
+				rm $f
+				mv $f $i
+			done
 			;;
 		m|M)
 			echo "Mandatory"
+			rm *_bonus.c *_bonus.h
 			;;
 		*)
 			echo "Error"
+			return
+			;;
 	esac
-
-	# rm $0
+	# removeDebug
 }
 
-echo "Removing script"
-rm $0
+cleanRepo
+# rm $0
